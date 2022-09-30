8ac0b5c15187c5cd5671219f3779760b54efa1e4
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-10-01 00:37:54 +0600

    reseted components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import Body from './utilities/component/Body/Body';
 import Nav from './utilities/component/Nav/Nav';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
-      <Body></Body>
     </div>
   );
 }
