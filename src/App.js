cb643f4d6224c7914c18979a6dfe0fc9734a8a3b
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-10-01 11:55:40 +0600

    reseting the whole site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Nav from './utilities/component/Nav/Nav';
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
